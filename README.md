3c26f3e0caf9b9e722d0d53d0fe7be1b7f595826
Joshua Ruddock <joshua.ruddock27@outlook.com>
2023-10-07 16:46:17 +0100

    Added some baseplate
    Removed README, modified README.md to be compliant with standards, added baseplate for python projects and web projects.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # projectInit
+
 Repo housing my project initialisation script and boilerplate code for any languages it supports.
---
+++ python/src/main.py
@@ -0,0 +1,11 @@
+# Import any necessary modules or libraries here
+import math
+
+#Define your main function
+def main():
+    #Your code goes here
+    print("Hello world!")
+
+#Check if this script is being run as the main program
+if __name__ == "__main__":
+    main()
