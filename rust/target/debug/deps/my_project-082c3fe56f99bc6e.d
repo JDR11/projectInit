63cd6f835c75bd965e8a76421eb7e32ebf9be75e
Joshua Ruddock <joshua.ruddock27@outlook.com>
2023-10-08 15:38:08 +0100

    Rust post test run
    Files modified and added by rust lang after a test run.

---
+++ rust/target/debug/deps/my_project-082c3fe56f99bc6e.d
@@ -0,0 +1,5 @@
+C:\Users\joshu\Documents\Personal\projectInit\rust\target\debug\deps\my_project-082c3fe56f99bc6e.rmeta: src\main.rs
+
+C:\Users\joshu\Documents\Personal\projectInit\rust\target\debug\deps\my_project-082c3fe56f99bc6e.d: src\main.rs
+
+src\main.rs:
