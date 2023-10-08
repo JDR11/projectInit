a538dcfc1accc4de542ed362bc6a004a17c3809c
Joshua Ruddock <joshua.ruddock27@outlook.com>
2023-10-08 13:45:30 +0100

    Added the final baseplate (for now)
    Added baseplate for node.js app and a rust project.

---
+++ rust/target/debug/deps/my_project.d
@@ -0,0 +1,5 @@
+C:\Users\joshu\Documents\Personal\projectInit\rust\target\debug\deps\my_project.exe: src\main.rs
+
+C:\Users\joshu\Documents\Personal\projectInit\rust\target\debug\deps\my_project.d: src\main.rs
+
+src\main.rs:
