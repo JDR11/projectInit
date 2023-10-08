84833d8837bb7286df0d4b58fd069caa01a409dc
Joshua Ruddock <joshua.ruddock27@outlook.com>
2023-10-08 13:14:39 +0100

    Added baseplate for more languages
    Added baseplate code for C, C++ and C# projects

---
+++ c++/main.cpp
@@ -0,0 +1,22 @@
+#include "header.h"
+
+// Function declaration (if needed)
+// void myFunction();
+
+int main()
+{
+    // Your code goes here
+    std::cout << "Hello World!" << std::endl;
+
+    // Call your functions or add more code as needed
+    // myFunction();
+
+    return 0; // Return 0 to indicate successful execution
+}
+
+// Exapmle of a function (if needed)
+/*
+void myFunction() {
+    // Function code here
+}
+*/
